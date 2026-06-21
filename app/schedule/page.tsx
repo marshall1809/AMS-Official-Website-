2f40d752f1ad7b992d1c6721caa8d777925e88b2
marshall1809 <marshall.private7@gmail.com>
2026-06-21 23:08:58 +0200

    Redirect removed public Schedule page

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,15 +1,5 @@
-import { PublicSchedulePage } from "@/components/cms/public-competition";
-import { currentPublicSeason } from "@/components/cms/public-teams";
-import { getCmsData } from "@/lib/cms/repository";
+import { permanentRedirect } from "next/navigation";
 
-export const metadata = {
-  title: "Schedule | Alliance Master Series",
-  description: "Match schedule for the current Alliance Master Series Season."
-};
-
-export const dynamic = "force-dynamic";
-
-export default async function CurrentSchedulePage() {
-  const data = await getCmsData();
-  return <PublicSchedulePage data={data} season={currentPublicSeason(data)} />;
+export default function RemovedSchedulePage() {
+  permanentRedirect("/bracket");
 }
